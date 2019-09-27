be5f426cf01effed90ba023a16bc89393d4864bf
bhanu420 <prakasheng0074@gmail.com>
2019-09-26 19:41:17 -0500

    My Edited Code

---
+++ src/ClassB.java
@@ -0,0 +1,10 @@
+
+public class ClassB {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        //Today is my good day
+		//I hope it will be same for tomorrow
+	}
+
+}
